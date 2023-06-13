e8b33dc255811c59a6c8ecc5bc11cf163b43a343
Firoz <zorif22@gmail.com>
2023-06-13 19:30:20 +0000

    Initial commit

---
+++ python.py
@@ -0,0 +1,12 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def add_numbers(a, b):
+    return a + b
+
+name = "John"
+print(greet(name))
+
+result = add_numbers(5, 3)
+print(f"The result is: {result}")
+
